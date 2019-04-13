f1a02a3264e5f57ad46ac57fab3c2bba67783c63
Shaban Fadil <ewizzard100@gmail.com>
2019-04-12 23:47:47 -0400

    Update archive.php
    added the_archive_title()

--- wp-content/themes/universitytutorial/archive.php
+++ wp-content/themes/universitytutorial/archive.php
@@ -2,12 +2,9 @@
 
 <div class="page-banner__bg-image" style="background-image:url(<?php echo get_theme_file_uri('images/ocean.jpg')?>);"></div>
     <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php if(is_category()){
-        single_cat_title();}else{
-        if(is_author()){
-          echo "Post by: "; the_author();
-        }
-        }?></h1>
+   
+
+<h1 class="page-banner__title"><?php the_archive_title();?></h1>
       <div class="page-banner__intro">
         <p>Don't forget to replace me later</p>
       </div>
