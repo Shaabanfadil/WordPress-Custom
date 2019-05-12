78561ab57b46a21d4a7212a71c65bf6d8b15da88
Shaban Fadil <ewizzard100@gmail.com>
2019-05-12 00:26:39 -0400

    theme correction

--- wp-content/themes/universitytutorial/single-professor.php
+++ wp-content/themes/universitytutorial/single-professor.php
@@ -17,7 +17,15 @@ while(have_posts()){
 
  
     <div class="generic-content">
+     <div class="row group">
+       <div class="one-third">
+       <?php the_post_thumbnail() ?>
+    
+     </div>
+    <div class="two-third">
     <?php the_content();?>
+    </div>
+    </div>
     </div>
   
    <!-- Loop through array post type field object-->
